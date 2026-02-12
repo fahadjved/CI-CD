53280a0f8476266ab2c8471d397178eb79d38123
Fahad Javed <26404@riphahfsd.edu.pk>
2026-02-12 22:28:53 +0500

    some cjhanges

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serverless = require("serverless-http");
 const app = express();
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello From lambda server !" });
+  res.json({ message: "Hello From lambda server form first time !" });
 });
 
 // ❌ NO app.listen
